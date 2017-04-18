b2c48cfdaddead1bcc61dfb49fd747bf4352a1e3
jirawatk <jirawatk@kkumail.com>
2017-04-18 21:45:39 +0700

    test

--- main.php
+++ main.php
@@ -1,14 +1,1 @@
-﻿<!doctype html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Groovin</title>
-
-
-</head>
-<body>
-<h1>helloasdsadsadsa</h1>
-
-
-</body>
-</html>
+﻿<?php include("index.php")?>
