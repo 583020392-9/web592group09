afd4eda774c9992d4ab491ee03e97a1da0926379
PuwitChanapan <puwitchanapan39@gmail.com>
2017-04-27 00:14:57 +0700

    rehere

--- main.php
+++ main.php
@@ -157,30 +157,88 @@
         </div>
     </section>
 
-    <!-- About Section -->
-    <section class="success" id="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2>About</h2>
-                    <hr class="star-light">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-lg-offset-2">
-                    <p>Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.</p>
-                </div>
-                <div class="col-lg-4">
-                    <p>Whether you're a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!</p>
-                </div>
-                <div class="col-lg-8 col-lg-offset-2 text-center">
-                    <a href="#" class="btn btn-lg btn-outline">
-                        <i class="fa fa-download"></i> Download Theme
-                    </a>
-                </div>
-            </div>
-        </div>
-    </section>
+    <section id="about" class="section">
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="heading">
+				<h3><span>About us</span></h3>
+			</div>
+			<div class="sub-heading">
+				<p>
+					 Creating By
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-3">
+			<div class="team-box">
+			<img src="img/dummies/te.jpg" alt=""  width="100%" height="280" />
+			<div class="roles">
+				<h5><strong>Jirawat  Khumsiri</strong></h5>
+				<p>
+					CEO - Founder
+				</p>
+				<ul class="social-profile">
+					<li><a href="https://www.facebook.com/jirawat.kumsiri/html/"><i class="fa fa-facebook fa-lg"></i></a></li>
+					<li><a href="https://twitter.com/search?f=users&vertical=default&q=jirawatzz"><i class="fa fa-twitter fa-lg"></i></a></li>
+					<li><a href="#"><i class="fa fa-dribbble fa-lg"></i></a></li>
+				</ul>
+			</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="team-box">
+			<img src="img/dummies/oat.jpg" alt=""  width="100%" height="280" />
+			<div class="roles">
+				<h5><strong>Puwit  Chanapan</strong></h5>
+				<p>
+					Lead designer
+				</p>
+				<ul class="social-profile">
+					<li><a href="https://www.facebook.com/oat.puwit/html/"><i class="fa fa-facebook fa-lg"></i></a></li>
+					<li><a href="https://twitter.com/search?f=users&vertical=default&q=%40o_puwit"><i class="fa fa-twitter fa-lg"></i></a></li>
+					<li><a href="#"><i class="fa fa-dribbble fa-lg"></i></a></li>
+				</ul>
+			</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="team-box">
+			<img src="img/dummies/ake.jpg" alt="" width="100%" height="280" />
+			<div class="roles">
+				<h5><strong>Ratthawit  Johnburee</strong></h5>
+				<p>
+					Customer support
+				</p>
+				<ul class="social-profile">
+					<li><a href="https://www.facebook.com/ake.ratthawit/html/"><i class="fa fa-facebook fa-lg"></i></a></li>
+					<li><a href="https://twitter.com/search?f=users&vertical=default&q=a_ratthawit"><i class="fa fa-twitter fa-lg"></i></a></li>
+					<li><a href="#"><i class="fa fa-dribbble fa-lg"></i></a></li>
+				</ul>
+			</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="team-box">
+			<img src="img/dummies/arm.jpg" alt=""  width="100%" height="280" />
+			<div class="roles">
+				<h5><strong>Nutthapon  Pitha</strong></h5>
+				<p>
+					Coffee maker
+				</p>
+				<ul class="social-profile">
+					<li><a href="https://www.facebook.com/armfreez/html/"><i class="fa fa-facebook fa-lg"></i></a></li>
+					<li><a href="https://twitter.com/search?f=users&vertical=default&q=4rmster"><i class="fa fa-twitter fa-lg"></i></a></li>
+					<li><a href="#"><i class="fa fa-dribbble fa-lg"></i></a></li>
+				</ul>
+			</div>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
 
     <!-- Contact Section -->
     <section id="contact">
